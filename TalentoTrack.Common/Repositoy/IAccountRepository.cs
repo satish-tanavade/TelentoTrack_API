084bed97f11bd3e134889e9cb61d2e4a80fe135b
satish-tanavade <satishtanavade6363@gmail.com>
2024-08-21 18:30:22 +0530

    -Initial Commit -Base structure implemented -Created Login web Api

---
+++ TalentoTrack.Common/Repositoy/IAccountRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TalentoTrack.Common.DTOs.Account;
+using TalentoTrack.Common.Entity;
+
+namespace TalentoTrack.Common.Repositoy
+{
+    public interface IAccountRepository
+    {
+        Task<LoginDetails> GetLoginDetails(string Username, string Password);
+    }
+}
---
+++ TalentoTrack.Dao/Repository/AccountRepository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TalentoTrack.Common.DTOs.Account;
+using TalentoTrack.Common.Entity;
+using TalentoTrack.Common.Repositoy;
+using TalentoTrack.Dao.Db;
+
+
+namespace TalentoTrack.Dao.Repository
+{
+    public class AccountRepository : IAccountRepository
+    {
+        public TalentoTrack_DbContext _dbContext;
+
+        public AccountRepository(TalentoTrack_DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<LoginDetails> GetLoginDetails(string Username, string Password)
+        {
+            var dbRecord = await _dbContext.tbl_login_details.Where(p => (p.Username != null && p.Username.Equals(Username!)) && (p.Password != null && p.Password.Equals(Password!))).FirstOrDefaultAsync();
+
+            return dbRecord!;
+        }
+    }
+}
